a82f273fa2ab4c62cdb077105c7a99d75eb1086e
Derrick Eberlein <popetrivium@gmail.com>
2024-10-02 19:01:16 -0400

    Removed Unused Imports

--- src/main/java/org/eyazahrid/util/SocialMedia/Reddit/RedditClient.java
+++ src/main/java/org/eyazahrid/util/SocialMedia/Reddit/RedditClient.java
@@ -1,14 +1,12 @@
 package org.eyazahrid.util.SocialMedia.Reddit;
 
 import com.google.gson.*;
-import com.google.gson.stream.JsonReader;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.*;
 
 public class RedditClient {
