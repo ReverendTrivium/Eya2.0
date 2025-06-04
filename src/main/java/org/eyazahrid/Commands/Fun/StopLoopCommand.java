1948423dfec262ffc7b566a9c528b4396a02e026
ReverendTrivium <popetrivium@gmail.com>
2025-06-04 11:32:59 -0400

    Added Ability to loop NSFW images in multiple channels at the same time.

--- src/main/java/org/eyazahrid/Commands/Fun/StopLoopCommand.java
+++ src/main/java/org/eyazahrid/Commands/Fun/StopLoopCommand.java
@@ -8,19 +8,33 @@ import org.eyazahrid.Eyazahrid;
 
 import java.util.Objects;
 
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
 public class StopLoopCommand extends Command {
 
     public StopLoopCommand(Eyazahrid bot) {
         super(bot);
         this.name = "stoploop";
-        this.description = "Stop the looping NSFW posts.";
+        this.description = "Stop NSFW loops in this channel or all channels.";
         this.permission = Permission.MANAGE_SERVER;
         this.category = Category.FUN;
+        this.args.add(new OptionData(OptionType.BOOLEAN, "all", "Set to true to stop all NSFW loops. Default is just this channel."));
     }
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        LoopNSFWCommand.stopLoop();
-        event.reply("Stopped looping NSFW posts.").setEphemeral(true).queue();
+        boolean stopAll = event.getOption("all") != null && Objects.requireNonNull(event.getOption("all")).getAsBoolean();
+        if (stopAll) {
+            LoopNSFWCommand.stopAllLoops();
+            event.reply("Stopped all NSFW loops across all channels.").setEphemeral(true).queue();
+        } else {
+            String channelId = event.getChannel().getId();
+            if (LoopNSFWCommand.stopLoop(channelId)) {
+                event.reply("Stopped NSFW loop in this channel.").setEphemeral(true).queue();
+            } else {
+                event.reply("No active NSFW loop was running in this channel.").setEphemeral(true).queue();
+            }
+        }
     }
 }
